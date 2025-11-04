2b4b5e1c3a44c9fe3152d308d956723c42919266
Suiseich <aarontayag89@gmail.com>
2025-11-04 13:35:15 +0800

    first commit

---
+++ src/main/java/com/tayag/facebookapi/FacebookapiApplication.java
@@ -0,0 +1,11 @@
+package com.tayag.facebookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookapiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(com.tayag.facebookapi.FacebookapiApplication.class, args);
+    }
+}
